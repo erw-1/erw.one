92776addb9ab22fe4f5168de02f725a24f858e1a
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-03-09 15:15:40 +0100

    Update leaflet_join_googlesheet.js

--- voirie/js/leaflet_join_googlesheet.js
+++ voirie/js/leaflet_join_googlesheet.js
@@ -1,16 +1,12 @@
 // Fonction pour charger la configuration générale
-function loadConfig() {
-    return fetch('config/config.json').then(response => response.json());
-}
+const loadConfig = () => fetch('config/config.json').then(response => response.json());
 
 // Fonction pour charger les types d'icônes
-function loadTypesConfig(iconConfigPath) {
-    return fetch(iconConfigPath).then(response => response.json());
-}
+const loadTypesConfig = (iconConfigPath) => fetch(iconConfigPath).then(response => response.json());
 
 // Fonction pour initialiser la carte
-function initMap(config) {
-    var map = L.map('map').setView(config.mapSettings.center, config.mapSettings.zoomLevel);
+const initMap = (config) => {
+    const map = L.map('map').setView(config.mapSettings.center, config.mapSettings.zoomLevel);
     L.tileLayer(config.tileLayerUrl, config.tileLayerOptions).addTo(map);
 
     Promise.all([
@@ -20,10 +16,10 @@ function initMap(config) {
         createLegend(map, typesConfig);
         addGeoJsonToMap(map, sheetData, typesConfig, config);
     });
-}
+};
 
 // Fonction pour charger les données du Google Sheets
-function loadSheetData(googleSheetUrl) {
+const loadSheetData = (googleSheetUrl) => {
     return fetch(googleSheetUrl)
         .then(response => response.text())
         .then(data => {
@@ -36,57 +32,58 @@ function loadSheetData(googleSheetUrl) {
                 lien: row.c[3] ? row.c[3].v : ''
             })).filter(item => item.code_dep);
         });
-}
+};
+
+// Créer une fonction pour générer le contenu de la popup
+const createPopupContent = (match, config) => {
+    return config.popupTemplate.replace(/\{(\w+)\}/g, (_, key) => match[key] || '');
+};
 
 // Fonction pour ajouter le GeoJSON à la carte avec style personnalisé
-function addGeoJsonToMap(map, sheetsData, typesConfig, config) {
+const addGeoJsonToMap = (map, sheetsData, typesConfig, config) => {
     fetch(config.geojsonFeature)
         .then(response => response.json())
         .then(data => {
             L.geoJson(data, {
                 style: config.geoJsonStyle,
-                onEachFeature: function (feature, layer) {
+                onEachFeature: (feature, layer) => {
                     const departmentCode = feature.properties.code;
                     const matches = sheetsData.filter(row => row[config.joinField].padStart(2, '0') === departmentCode);
-
-                    if (!clusterGroupsByDepartment[departmentCode]) {
-                        clusterGroupsByDepartment[departmentCode] = new L.MarkerClusterGroup();
-                        map.addLayer(clusterGroupsByDepartment[departmentCode]);
-                    }
+                    const clusterGroup = clusterGroupsByDepartment[departmentCode] || new L.MarkerClusterGroup();
 
                     matches.forEach(match => {
-                        const customIcon = getCustomIcon(match.type, typesConfig);
-                        const popupContent = config.popupTemplate.replace(/\{(\w+)\}/g, function(_, key) {
-                            return match[key] || '';
-                        });
-                        const marker = L.marker(layer.getBounds().getCenter(), { icon: customIcon }).bindPopup(popupContent);
-                        clusterGroupsByDepartment[departmentCode].addLayer(marker);
+                        const marker = L.marker(layer.getBounds().getCenter(), { icon: getCustomIcon(match.type, typesConfig) })
+                            .bindPopup(createPopupContent(match, config));
+                        clusterGroup.addLayer(marker);
                     });
+
+                    clusterGroupsByDepartment[departmentCode] = clusterGroup;
+                    map.addLayer(clusterGroup);
                 }
             }).addTo(map);
         }).catch(error => {
             console.error('Erreur lors de l’ajout du GeoJSON à la carte :', error);
         });
-}
+};
 
 // Fonction pour créer la légende avec le fichier de configuration
-function createLegend(map, typesConfig) {
-    var legend = L.control({ position: 'bottomright' });
+const createLegend = (map, typesConfig) => {
+    const legend = L.control({ position: 'bottomright' });
 
-    legend.onAdd = function () {
-        var div = L.DomUtil.create('div', 'info legend');
+    legend.onAdd = () => {
+        const div = L.DomUtil.create('div', 'info legend');
         div.innerHTML += '<div class="title">Fonction des outils</div>';
-        typesConfig.forEach(function (configItem) {
+        typesConfig.forEach(configItem => {
             div.innerHTML += `<i style="background-image: url(${configItem.icon}); background-repeat: no-repeat; background-position: center center;"></i><span>${configItem.type}</span><br>`;
         });
         return div;
     };
 
     legend.addTo(map);
-}
+};
 
 // Fonction pour obtenir une icône personnalisée en fonction du type
-function getCustomIcon(type, typesConfig) {
+const getCustomIcon = (type, typesConfig) => {
     const iconInfo = typesConfig.find(t => t.type === type) || typesConfig.find(t => t.type === 'Autre');
     return L.icon({
         iconUrl: iconInfo.icon,
@@ -94,10 +91,10 @@ function getCustomIcon(type, typesConfig) {
         iconAnchor: [12, 41],
         popupAnchor: [1, -34],
     });
-}
+};
 
 // Limite de la clusterisation au département
-var clusterGroupsByDepartment = {};
+const clusterGroupsByDepartment = {};
 
 // Charger la configuration et initialiser la carte
 loadConfig().then(initMap);
