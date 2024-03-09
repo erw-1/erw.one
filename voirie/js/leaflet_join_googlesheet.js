b458a9ed99c24e4d5510a3ebf27ca3705d24d0e0
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-03-09 10:43:19 +0100

    Update leaflet_join_googlesheet.js

--- voirie/js/leaflet_join_googlesheet.js
+++ voirie/js/leaflet_join_googlesheet.js
@@ -1,1 +1,27 @@
+// Initialiser la carte
+var map = L.map('map').setView([48.8566, 2.3522], 13);
 
+// Ajouter un calque de tuiles à la carte
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Ajouter le GeoJSON
+var geojsonFeature = '/data/dpt.geojson'; // Mettez le chemin vers votre fichier GeoJSON
+
+// Utiliser AJAX pour charger le GeoJSON externe
+var xhr = new XMLHttpRequest();
+xhr.open('GET', geojsonFeature, true);
+xhr.responseType = 'json';
+xhr.onload = function() {
+    if (xhr.status === 200) {
+        var data = xhr.response;
+        L.geoJson(data, {
+            onEachFeature: function (feature, layer) {
+                layer.bindPopup(feature.properties.nom);
+            }
+        }).addTo(map);
+    }
+};
+xhr.send();
