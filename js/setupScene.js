89ab639956b853a67eaa024c422de8a41dfd6ec8
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-04-15 13:22:14 +0200

    Update setupScene.js

--- js/setupScene.js
+++ js/setupScene.js
@@ -1,3 +1,5 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.1/build/three.module.js';
+
 export function initScene() {
     const scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
