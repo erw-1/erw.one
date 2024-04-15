fb9121089dbbf36aa3930efa112cd0cbf44f95cb
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-04-15 09:42:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.1/build/three.module.js';
 import { initScene } from './setupScene.js';
 import { setupPostProcessing } from './postProcessing.js';
 import { addInteraction } from './interaction.js';
