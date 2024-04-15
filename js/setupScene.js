91d1bd2a67498a21dd4b3a7790fa2b611df9abab
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-04-15 13:17:56 +0200

    Update setupScene.js

--- js/setupScene.js
+++ js/setupScene.js
@@ -1,14 +1,19 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.1/build/three.module.js';
-
 export function initScene() {
     const scene = new THREE.Scene();
-    
     const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.z = 500;
-    
     const renderer = new THREE.WebGLRenderer();
     renderer.setSize(window.innerWidth, window.innerHeight);
-    
     document.body.appendChild(renderer.domElement);
+
+    // Handle window resize:
+    window.addEventListener('resize', onWindowResize, false);
+
+    function onWindowResize() {
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+        renderer.setSize(window.innerWidth, window.innerHeight);
+    }
+
     return { scene, camera, renderer };
 }
