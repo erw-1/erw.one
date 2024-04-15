354af0fddea40d4b1da0a0391efb3c76dc137a4c
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-04-15 09:21:57 +0200

    Create setupScene.js

---
+++ js/setupScene.js
@@ -0,0 +1,11 @@
+import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.126.1/build/three.module.js';
+
+export function initScene() {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
+
+    return { scene, camera, renderer };
+}
