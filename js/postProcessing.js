20ffc77e330d30ae901ffafb0f9738717a35f730
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-04-15 11:37:38 +0200

    Update postProcessing.js

--- js/postProcessing.js
+++ js/postProcessing.js
@@ -9,9 +9,9 @@ export function setupPostProcessing(renderer, scene, camera) {
     composer.addPass(renderPass);
     const bloomPass = new UnrealBloomPass(
         new THREE.Vector2(window.innerWidth, window.innerHeight), // resolution
-        3, // strength
-        2, // radius
-        0 // threshold
+        5, // strength
+        1, // radius
+        0.01 // threshold
     );
     composer.addPass(bloomPass);
 
