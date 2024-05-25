e67ababd7dda178443ad478ce08bc3053e632ca4
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-05-25 17:12:09 +0200

    Update leaflet_join_googlesheet.js

--- voirie/js/leaflet_join_googlesheet.js
+++ voirie/js/leaflet_join_googlesheet.js
@@ -102,7 +102,7 @@ const createLegend = (map, typesConfig) => {
 
     legend.onAdd = () => {
         const div = L.DomUtil.create('div', 'info legend');
-        div.innerHTML += '<div class="title">Fonction des cartes</div>';
+        div.innerHTML += '<div class="title">Fonction des cartes interactives</div>';
         typesConfig.forEach(configItem => {
             div.innerHTML += `<i style="background-image: url(${configItem.icon}); background-repeat: no-repeat; background-position: center center;"></i><span>${configItem.type}</span><br>`;
         });
@@ -172,7 +172,7 @@ const initMap = (config, unmatchedEntriesCallback) => {
     const titleControl = L.control({ position: 'topleft' });
     titleControl.onAdd = function(map) {
         const div = L.DomUtil.create('div', 'map-title');
-        div.innerHTML = '<h1>Inventaire des cartes départementales publiques de communication sur les routes</h1>';
+        div.innerHTML = '<h1>Inventaire des cartes départementales de communication sur les routes</h1>';
         return div;
     };
     titleControl.addTo(map);
