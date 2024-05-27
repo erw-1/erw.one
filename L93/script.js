2851bf9f40d4f39beb68f9a010af2e4669e0a68e
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-05-27 09:38:28 +0200

    Update script.js

--- L93/script.js
+++ L93/script.js
@@ -1,43 +1,43 @@
-// Define Lambert93 Projection
+// Définition de la projection Lambert93
 proj4.defs("EPSG:2154","+proj=lcc +lat_1=44.100000 +lat_2=49.200000 +lat_0=46.800000 +lon_0=3.000000 +x_0=700000 +y_0=6600000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs");
 var lambert93 = new L.Proj.CRS('EPSG:2154',
     proj4.defs("EPSG:2154"),
     {
-        resolutions: [8192, 4096, 2048, 1024, 512, 256, 128], // Adjust according to needs
+        resolutions: [8192, 4096, 2048, 1024, 512, 256, 128],
         origin: [0, 0]
     }
 );
 
-// Initialize the map
+// Initialisation de la carte avec la projection personnalisée
 var map = L.map('map', {
     crs: lambert93,
     continuousWorld: true,
     worldCopyJump: false,
 });
 
-// Set the view to a given place and zoom
+// Définition de la vue initiale (ajuster selon le besoin)
 map.setView([46.52863469527167, 2.43896484375], 5);
 
-// Base Layer
-var osm = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
-    attribution: 'Map data © OpenStreetMap contributors'
+// Couche de base CartoDB sans étiquettes
+var baseLayer = L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
+    attribution: 'Map data © OpenStreetMap contributors, CartoDB'
 }).addTo(map);
 
-// Function to handle click events on the map
+// Gestion des événements de clic sur la carte
 map.on('click', function(e) {
     var coords = e.latlng;
     var transformedCoords = proj4('EPSG:4326', 'EPSG:2154', [coords.lng, coords.lat]);
 
-    // Create a popup at the location of the click
+    // Création d'un popup au point de clic
     L.popup()
         .setLatLng(coords)
         .setContent("Coordonnées Lambert93 : " + transformedCoords.join(', '))
         .openOn(map);
 
-    // Copy to clipboard
+    // Copie des coordonnées dans le presse-papiers
     navigator.clipboard.writeText(transformedCoords.join(', ')).then(function() {
-        console.log('Coordinates copied to clipboard successfully!');
+        console.log('Coordonnées copiées avec succès !');
     }, function(err) {
-        console.error('Could not copy coordinates: ', err);
+        console.error('Erreur lors de la copie des coordonnées : ', err);
     });
 });
