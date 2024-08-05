f5a3a7195771d0e1d738c5feabc05273e1d5e7b1
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-08-05 11:49:18 +0200

    fix layer order

--- apps/LRS/script.js
+++ apps/LRS/script.js
@@ -1,17 +1,3 @@
-// Initialize the map
-var map = L.map('map', {
-    center: [47.6205, 6.3498], // Set to the desired center coordinates
-    zoom: 10,                  // Set to the desired initial zoom level
-    zoomControl: false         // Disables the default zoom controls
-}).setView([47.6205, 6.3498], 10);
-
-// Add OpenStreetMap tiles
-L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
-    attribution: 'HSN | OSM',
-    maxNativeZoom: 19,
-    maxZoom: 22
-}).addTo(map);
-
 // Style function for routes70 layer (dark grey lines)
 function routes70Style(feature) {
     return {
@@ -31,18 +17,33 @@ function pr70Style(feature) {
     };
 }
 
+// Initialize the map
+var map = L.map('map', {
+    center: [47.6205, 6.3498], // Set to the desired center coordinates
+    zoom: 10,                  // Set to the desired initial zoom level
+    zoomControl: false         // Disables the default zoom controls
+}).setView([47.6205, 6.3498], 10);
+
+// Add cartodb tiles
+L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {
+    attribution: 'HSN | OSM',
+    maxNativeZoom: 19,
+    maxZoom: 22
+}).addTo(map);
+
 // Fetch and add the second layer (routes70.geojson) with dark grey line styling
 fetch('data/routes70.geojson')
     .then(response => response.json())
     .then(data => L.geoJson(data, {
         style: routes70Style
-    }).addTo(map));
-
-// Fetch and add the first layer (pr70.geojson) with red dot styling
-fetch('data/pr70.geojson')
-    .then(response => response.json())
-    .then(data => L.geoJson(data, {
-        pointToLayer: function (feature, latlng) {
-            return L.circleMarker(latlng, pr70Style(feature));
-        }
-    }).addTo(map));
+    }).addTo(map))
+    .then(() => {
+        // Fetch and add the first layer (pr70.geojson) with red dot styling
+        fetch('data/pr70.geojson')
+            .then(response => response.json())
+            .then(data => L.geoJson(data, {
+                pointToLayer: function (feature, latlng) {
+                    return L.circleMarker(latlng, pr70Style(feature));
+                }
+            }).addTo(map));
+    });
