eea7d1e5e6be6de945ac1c97a41549765d52014d
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-08-06 11:37:07 +0200

    simplify

--- apps/LRS/script.js
+++ apps/LRS/script.js
@@ -1,44 +1,29 @@
 // Initialize the map
-const map = L.map('map', {center: [47.6205, 6.3498], zoom: 10, zoomControl: false}).setView([47.6205, 6.3498], 10);
+const map = L.map('map', {center: [47.6205, 6.3498], zoom: 10, zoomControl: false});
 L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {attribution: 'HSN | OSM', maxNativeZoom: 19, maxZoom: 22}).addTo(map);
 
-// Create Panes
-['routesPane', 'pointsPane', 'previewPane'].forEach((pane, index) => {
-  map.createPane(pane).style.zIndex = 400 + (index * 100);
+// Create Panes in order from bottom to top
+['routesPane', 'pointsPane'].forEach((pane, index) => {
+  map.createPane(pane).style.zIndex = 400 + (index);
 });
 
 // Define styles
 const styles = {
-  route: { color: "#4d4d4d", weight: 2, opacity: 0.8, pane: 'routesPane' },
+  route: { color: "#4d4d4d", weight: 2, opacity: 1, pane: 'routesPane' },
   point: { radius: 3, fillColor: "#ff0000", color: "none", fillOpacity: 1, pane: 'pointsPane' }
 };
 
 // Function to add GeoJSON layers to the map
-const addGeoJsonLayer = (data, style, pointToLayer) => {
-  L.geoJson(data, { style, pointToLayer }).addTo(map);
+const addGeoJsonLayer = (url, style, pointToLayer) => {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => L.geoJson(data, { style, pointToLayer }).addTo(map))
 };
 
 // Initialize the map with data
-const initializeMap = async () => {
-  try {
-    const [routesResponse, prResponse] = await Promise.all([
-      fetch('data/routes70.geojson'),
-      fetch('data/pr70.geojson')
-    ]);
-
-    const [routesData, prData] = await Promise.all([
-      routesResponse.json(),
-      prResponse.json()
-    ]);
-
-    // Add routes layer
-    addGeoJsonLayer(routesData, styles.route);
-
-    // Add points layer with custom point styling
-    addGeoJsonLayer(prData, null, (feature, latlng) => L.circleMarker(latlng, styles.point));
-  } catch (error) {
-    console.error('Error loading GeoJSON data:', error);
-  }
+const initializeMap = () => {
+  addGeoJsonLayer('data/routes70.geojson', styles.route);
+  addGeoJsonLayer('data/pr70.geojson', null, (feature, latlng) => L.circleMarker(latlng, styles.point));
 };
 
 // Call the initialize function
