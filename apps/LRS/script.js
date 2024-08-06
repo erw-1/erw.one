7fa032c1545d803521844882aacb20d1f04d5f9c
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2024-08-06 12:07:23 +0200

    add zoom treshold and refactor

--- apps/LRS/script.js
+++ apps/LRS/script.js
@@ -1,6 +1,6 @@
 // Initialize the map
-const map = L.map('map', {center: [47.6205, 6.3498], zoom: 10, zoomControl: false});
-L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', {attribution: 'HSN | OSM', maxNativeZoom: 19, maxZoom: 22}).addTo(map);
+const map = L.map('map', { center: [47.6205, 6.3498], zoom: 10, zoomControl: false });
+L.tileLayer('https://cartodb-basemaps-a.global.ssl.fastly.net/light_nolabels/{z}/{x}/{y}.png', { attribution: 'HSN | OSM', maxNativeZoom: 19, maxZoom: 22 }).addTo(map);
 
 // Create Panes in order from bottom to top
 ['routesPane', 'pointsPane'].forEach((pane, index) => {
@@ -19,39 +19,47 @@ const simplifyGeometry = (geojson, tolerance) => {
 };
 
 // Function to add GeoJSON layers to the map
-const addGeoJsonLayer = (url, style, pointToLayer, simplify = false) => {
+const addGeoJsonLayer = (url, style, pointToLayer, simplify = false, layerVar) => {
   fetch(url)
     .then(response => response.json())
     .then(data => {
-      if (simplify) { // Check if simplification is needed
+      if (simplify) { // Simplify geometry if needed
         const tolerance = 1 / Math.pow(2, map.getZoom()); // Adjust tolerance based on zoom level
         data = simplifyGeometry(data, tolerance);
       }
-      L.geoJson(data, { style, pointToLayer }).addTo(map);
+      // Add new layer to the map
+      const layer = L.geoJson(data, { style, pointToLayer }).addTo(map);
+      // Manage global layer reference : if already exists, remove the old one
+      if (layerVar) {
+        if (window[layerVar]) {
+          map.removeLayer(window[layerVar]);
+        }
+        window[layerVar] = layer;
+      }
     })
     .catch(error => console.error('Error loading GeoJSON data:', error));
 };
 
-// Function to remove existing routes layer
-const removeRoutesLayer = () => {
-  map.eachLayer((layer) => {
-    if (layer.options && layer.options.pane === 'routesPane') {
-      map.removeLayer(layer);
-    }
-  });
-};
-
 // Initialize the map with data
 const initializeMap = () => {
-  addGeoJsonLayer('data/routes70.geojson', styles.route, null, true); // true = simplify the routes layer
-  addGeoJsonLayer('data/pr70.geojson', null, (feature, latlng) => L.circleMarker(latlng, styles.point));
+  addGeoJsonLayer('data/routes70.geojson', styles.route, null, true, 'routesLayer'); // Simplify and add routes layer
+  addGeoJsonLayer('data/pr70.geojson', null, (feature, latlng) => L.circleMarker(latlng, styles.point)); // Add points layer
 };
 
-// Update routes layer on zoom end to simplify geometries
+// Function to toggle points pane visibility based on zoom level
+const togglePointsPaneVisibility = () => {
+  const pointsPane = map.getPane('pointsPane');
+  pointsPane.style.display = map.getZoom() >= 14 ? 'block' : 'none';
+};
+
+// Update routes layer and points pane visibility on zoom end
 map.on('zoomend', () => {
-  removeRoutesLayer(); // Remove existing routes layer
-  addGeoJsonLayer('data/routes70.geojson', styles.route, null, true); // Simplify the routes layer on zoom end
+  addGeoJsonLayer('data/routes70.geojson', styles.route, null, true, 'routesLayer'); // Simplify and update routes layer
+  togglePointsPaneVisibility(); // Toggle points pane visibility
 });
 
-// Call the initialize function
+// Call the initialize function to load initial layers
 initializeMap();
+
+// Ensure correct initial visibility of points pane on map load
+map.on('load', togglePointsPaneVisibility);
