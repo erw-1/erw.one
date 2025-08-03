9391273cbc1cf8c490c83e0562fb215a9f2a464c
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2025-08-03 10:38:03 +0200

    Create config.js

---
+++ apps/km/config.js
@@ -0,0 +1,10 @@
+window.CONFIG = {
+  // Appears above the search box and in the <title>
+  TITLE   : '⟪ Title of your wiki ⟫',
+
+  // Any png / ico / svg file you like
+  FAVICON : 'icons/wiki32.png',
+
+  // Raw Markdown bundle (one file, many pages)
+  MD_LINK : 'https://hackmd.io/@h-s-n/doc_test/download'
+};
