4cf161140eb4d04d2bbef407b155a2ab239bec52
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2025-08-06 16:51:56 +0200

    Update config.js

--- apps/km/config.js
+++ apps/km/config.js
@@ -2,7 +2,7 @@ window.CONFIG = {
   // Appears above the search box and in the <title>
   TITLE   : 'Title of your wiki',
 
-  // Raw Markdown bundle (one file, many pages)
+  // Raw Markdown location (one file, many pages) can be 'content.md' next to index.html for instance
   MD : 'https://hackmd.io/@h-s-n/doc_test/download',
 
   // List every Highlight.js language you want available
