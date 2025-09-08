0842e10fb521ed40539f7be4068d6fbb7245c2da
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2025-09-08 13:27:26 +0200

    Update km_testing.js

--- apps/km/km_testing.js
+++ apps/km/km_testing.js
@@ -445,7 +445,7 @@ KM.ensureMarkdown = () => {
         const md = new marked.Marked()
             .use(alertMod.default())
             .use(footnoteMod.default())
-            .use(emojiMod.default({emojis: { heart: '❤️', tada: '🎉' },renderer: (token) => token.emoji}));
+            .use(emojiMod.default());
 
         return { parse: (src, opt) => md.parse(src, { ...opt, mangle: false }) };
     });
