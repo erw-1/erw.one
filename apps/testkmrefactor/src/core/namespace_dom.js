b4a0c44e81d1561c08b9836ac475acae5615dc66
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2025-09-10 19:37:54 +0200

    Update and rename namespace.js to namespace_dom.js

--- apps/testkmrefactor/src/core/namespace_dom.js
+++ apps/testkmrefactor/src/core/namespace_dom.js
@@ -1,4 +1,22 @@
 /* eslint-env browser, es2022 */
+export const whenIdle = (cb, timeout = 1500) =>
+  'requestIdleCallback' in window
+    ? requestIdleCallback(cb, { timeout })
+    : setTimeout(cb, 0);
+
+export const domReady = () =>
+  DOC.readyState !== 'loading'
+    ? Promise.resolve()
+    : new Promise(res =>
+        DOC.addEventListener('DOMContentLoaded', res, { once: true })
+      );
+
+export const clearSelection = () => {
+  const sel = window.getSelection?.();
+  if (sel && !sel.isCollapsed) sel.removeAllRanges();
+};
+
+export const baseURLNoHash = () => location.href.replace(/#.*$/, '');
 export const KM = (window.KM = window.KM || {});
 
 const DOC = document;
