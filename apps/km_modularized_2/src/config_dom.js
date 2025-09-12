048317f04e603a292315870532cc532596e92764
Erwan Vinot <127387189+erw-1@users.noreply.github.com>
2025-09-12 13:19:57 +0200

    Update config_dom.js

--- apps/km_modularized_2/src/config_dom.js
+++ apps/km_modularized_2/src/config_dom.js
@@ -118,7 +118,7 @@ export async function copyText(txt, node) {
 }
 
 /** Tiny inline SVG icon button factory */
-const ICONS = {
+export const ICONS_PUBLIC = {
   link: 'M3.9 12c0-1.7 1.4-3.1 3.1-3.1h5.4v-2H7c-2.8 0-5 2.2-5 5s2.2 5 5 5h5.4v-2H7c-1.7 0-3.1-1.4-3.1-3.1zm5.4 1h6.4v-2H9.3v2zm9.7-8h-5.4v2H19c1.7 0 3.1 1.4 3.1 3.1s-1.4 3.1-3.1 3.1h-5.4v2H19c2.8 0 5-2.2 5-5s-2.2-5-5-5z',
   code: 'M19,21H5c-1.1,0-2-0.9-2-2V7h2v12h14V21z M21,3H9C7.9,3,7,3.9,7,5v12 c0,1.1,0.9,2,2,2h12c2.2,0,2-2,2-2V5C23,3.9,22.1,3,21,3z M21,17H9V5h12V17z'
 };
@@ -131,3 +131,4 @@ export const iconBtn = (title, path, cls, onClick) =>
     ...(onClick && { onclick: onClick }),
     innerHTML: `<svg width="14" height="14" viewBox="0 0 24 24" aria-hidden="true"><path fill="currentColor" d="${path}"></path></svg>`
   });
+
